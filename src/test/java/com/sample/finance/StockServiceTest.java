ef13a44389aaa959040474499a121c38fa471f9e
Murali Padavala <mpadavala@gmail.com@>
2017-02-06 15:18:25 -0800

    new changes

---
+++ src/test/java/com/sample/finance/StockServiceTest.java
@@ -0,0 +1,5 @@
+package com.sample.finance;
+
+public class StockServiceTest {
+
+}
---
+++ src/test/java/com/sample/finance/TickerServiceTest.java
@@ -0,0 +1,52 @@
+package com.sample.finance;
+
+
+/*
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.bucephalus.dao.TickerDao;
+import com.bucephalus.dto.Ticker;
+import com.bucephalus.service.TickerService;
+
+*/
+public class TickerServiceTest {
+/*
+	private static final Logger logger = Logger.getLogger(TickerServiceTest.class.getName());
+	
+	private static TickerService service;
+	
+	@BeforeClass
+	public static void doInit(){
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		TickerDao dao = (TickerDao) context.getBean("tickerDao");
+		service = new TickerService();
+		service.setTickerDao(dao);
+	}
+		
+	@Test
+	public void loadTickers(){
+		service.loadTickers(true, "/Users/mpadavala/tickers/usa");
+	}
+	
+	
+	@Test
+	public void getTickers(){
+		try {
+			List<Ticker> tickers = service.getTickers();
+			for(Ticker ticker : tickers){
+				logger.info(ticker.toString());
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	*/
+}
